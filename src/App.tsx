9747e732684aea8d6e34be888bdb066a2cfe9ded
Avinash Gupta <avinash.gupta@vils.ai>
2024-02-21 22:02:33 +0530

    Layout Designing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-// import FeedPage from './pages/app/Feed';
-import LoginPage from './pages/auth/Login';
 import Layout from './layout';
-import SignupPage from './pages/auth/Signup';
 import FeedPage from './pages/app/Feed';
+
 const App = () => {
     return (
         <Layout>
